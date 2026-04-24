8e7f846c6b3c41579e64612ca2e3c48594133758
JoelZzc <joelz_23@alu.uabcs.mx>
2026-04-24 14:52:03 -0700

    feat: add product creation endpoint (POST /products)

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
         return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+    
+        return response()->json($product, 201);
     }
 }
