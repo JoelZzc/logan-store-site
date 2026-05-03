4dfbc0b1f0ea2a40d296da715a9eedbbda87ecbf
JoelZzc <joelz_23@alu.uabcs.mx>
2026-05-03 16:24:30 -0700

    feat: add authentication with Laravel Sanctum

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validate = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validate['name'],
+            'email' => $validate['email'],
+            'password' => Hash::make($validate['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(Request $request){
+        $validate = $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt(['email' => $validate['email'], 'password' => $validate['password']])) {
+            return response()->json(['message' => 'Credenciales incorrectas'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token]);
+    }
+
+    public function logout(Request $request){
+        
+        $request->user()->currentAccessToken()->delete();
+    
+        return response()->json(['message' => 'Sesión cerrada correctamente']);
+    }
+}
