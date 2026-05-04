b067c21ca832776f84a39060ac53951ad84c6309
JoelZzc <joelz_23@alu.uabcs.mx>
2026-05-03 18:34:04 -0700

    feat: add Order and OrderItem models with relationships

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+/** @var \App\Models\User $user */
 
 
 class AuthController extends Controller
@@ -49,7 +50,7 @@ class AuthController extends Controller
     }
 
     public function logout(Request $request){
-        
+
         $request->user()->currentAccessToken()->delete();
     
         return response()->json(['message' => 'Sesión cerrada correctamente']);
